9774667ab44b2a121822c520d62d3024b8290744
N S <neil.scott2@virgin.net>
2020-04-06 15:57:05 +0100

    Added bash scripts

---
+++ revert.sh
@@ -0,0 +1,6 @@
+# Bash script to revert to a backup of the previosu config.js 
+#
+#
+
+sudo cp config.js config.js.technojam.revert.backup
+sudo cp config.js.technojam.revert config.js
---
+++ setup.sh
@@ -0,0 +1,6 @@
+# Bash script to backup any existing config.js and copy the technojam one 
+#
+#
+
+sudo cp config.js config.js.technojam.backup
+sudo cp config.js.technojam config.js
